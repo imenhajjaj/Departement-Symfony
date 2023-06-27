556fe68eb9f22be146845e454cc3608344b1e9a6
imenhajjaj <96117949+imenhajjaj@users.noreply.github.com>
2023-06-27 12:01:08 +0100

    Add files via upload

---
+++ src/Entity/Employe.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EmployeRepository::class)
+ */
+class Employe
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0)
+     */
+    private $salaire;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $bornat;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Departement::class, inversedBy="employes")
+     */
+    private $departement;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getSalaire(): ?string
+    {
+        return $this->salaire;
+    }
+
+    public function setSalaire(string $salaire): self
+    {
+        $this->salaire = $salaire;
+
+        return $this;
+    }
+
+    public function getBornat(): ?\DateTimeInterface
+    {
+        return $this->bornat;
+    }
+
+    public function setBornat(\DateTimeInterface $bornat): self
+    {
+        $this->bornat = $bornat;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDepartement(): ?departement
+    {
+        return $this->departement;
+    }
+
+    public function setDepartement(?departement $departement): self
+    {
+        $this->departement = $departement;
+
+        return $this;
+    }
+}
